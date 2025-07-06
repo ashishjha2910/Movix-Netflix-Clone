7ec48afadfde1af5013805c2129a9c9564e4b8a4
ashishjha2910 <ashishdss258@gmail.com>
2025-07-06 11:47:27 +0530

    Initial commit: Movix Netflix Clone UI

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">🎬 Movix</header>
+      <main>
+        <h2>Top Movies</h2>
+        <p>Real-time movie listings using TMDB API coming soon...</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
